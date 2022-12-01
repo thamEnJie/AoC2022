67c496a883e172c8859b9fa3d5842f959f939ebb
thamEnJie <89586587+thamEnJie@users.noreply.github.com>
2022-12-02 01:06:00 +0800

    Day 1

--- Day 1/A_Code.py
+++ Day 1/A_Code.py
@@ -1,2 +1,4 @@
 with open('A_Input.txt') as file:
     puzzleInput = file.read()
+
+print(max([sum([int(y) for y in x.split("\n")]) for x in puzzleInput.split("\n\n")]))
--- Day 1/B_Code.py
+++ Day 1/B_Code.py
@@ -1,2 +1,4 @@
-with open('B_Input.txt') as file:
+with open('A_Input.txt') as file:
     puzzleInput = file.read()
+
+print(sum(sorted([sum([int(y) for y in x.split("\n")]) for x in puzzleInput.split("\n\n")])[-3:]))
