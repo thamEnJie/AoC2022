e0591a9dd8bde9ee7fff54ac05a50ca90319b386
thamEnJie <89586587+thamEnJie@users.noreply.github.com>
2022-12-03 00:18:06 +0800

    Day 2

--- Day 2/A_Code.py
+++ Day 2/A_Code.py
@@ -1,2 +1,15 @@
 with open('A_Input.txt') as file:
     puzzleInput = file.read()
+
+points = 0
+ref = "123"
+ref2 = "231"
+
+for i in puzzleInput.replace("A", '1').replace("B", '2').replace("C", '3').replace("X", '1').replace("Y", '2').replace(r"Z", '3').split("\n"):
+    if i[0] == i[-1]:
+        points += 3
+    elif ref[ref2.index(i[-1])] == i[0]:
+        points += 6
+    points += int(i[-1])
+
+print(points)
--- Day 2/B_Code.py
+++ Day 2/B_Code.py
@@ -1,2 +1,20 @@
-with open('B_Input.txt') as file:
+with open('A_Input.txt') as file:
     puzzleInput = file.read()
+
+points = 0
+ref = "ABC"
+ref2 = "BCA"
+
+for i in puzzleInput.split("\n"):
+    print(i)
+    j = ref.index(i[0])
+    if i[-1] == "Y":
+        points += 3 + j + 1
+    elif i[-1] == "Z":
+        points += 6 + ref.index(ref2[j]) + 1
+    else:
+        points += ref2.index(i[0]) + 1
+    print(points)
+    print()
+
+print(points)
